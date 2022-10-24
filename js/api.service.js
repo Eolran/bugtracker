eb68c67f537d03dd86cafddb174cf46cd2659dd9
Eolran <nevadasmith147@gmail.com>
2022-10-24 11:10:37 +0100

    function(Login & password)

--- js/api.service.js
+++ js/api.service.js
@@ -1,11 +1,53 @@
 //http://greenvelvet.alwaysdata.net/bugTracker/api/
 
 fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/ping")
-    .then(function(response) {
+    .then(function (response) {
         return response.json()
     })
-    .then(function(result){
+    .then(function (result) {
         JSON.stringify(result)
         console.log(result.result)
     })
     .catch(error => console.log('error', error));
+
+async function Login() {
+    let loginInput = document.getElementById("loginInput")
+    let passwordInput = document.getElementById("passwordInput")
+
+    await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/login/"+ loginInput + "/"+ passwordInput)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (result) {
+        JSON.stringify(result)
+        console.log(result.result)
+    })
+    .catch(error => console.log('error', error));
+}
+
+//Fonction Inscription
+function checkPassword() {
+    let passwordInput = document.getElementById("passwordInput")
+    let checkPasswordInput = document.getElementById("checkPasswordInput")
+
+    if (passwordInput == checkPasswordInput) {
+        Signup;
+    } else {
+        alert("Les mots de passe ne sont pas identiques. Veuillez vérifier.")
+    }
+    
+}
+async function Signup() {
+    let loginInput = document.getElementById("loginInput")
+    let passwordInput = document.getElementById("passwordInput")
+
+    await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/signup/"+ loginInput + "/"+ passwordInput)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (result) {
+        JSON.stringify(result)
+        console.log(result.result)
+    })
+    .catch(error => console.log('error', error));
+}
