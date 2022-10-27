1c6868fdbabc6fd12ec854c901491039e0cd7ae4
Eolran <nevadasmith147@gmail.com>
2022-10-27 10:12:26 +0100

    giga big commit

--- js/api.service.js
+++ js/api.service.js
@@ -12,8 +12,10 @@ fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/ping")
 
 //Fonction connexion
 async function Login() {
-    let loginInput = document.getElementById("loginInput").value
-    let passwordInput = document.getElementById("passwordInput").value
+    let loginInput = document.getElementById("loginInput").value;
+    let passwordInput = document.getElementById("passwordInput").value;
+
+    localStorage.setItem("username", loginInput);
 
     await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/login/"+ loginInput + "/"+ passwordInput)
     .then(function (response) {
@@ -25,6 +27,7 @@ async function Login() {
 
         let token = result.result.token;
         localStorage.setItem("token", token);
+        localStorage.setItem("ID", result.result.id);
 
         if (token) {
             window.location ="./list.html"
@@ -59,6 +62,7 @@ async function Signup() {
         
         let token = result.result.token;
         localStorage.setItem("token", token);
+        localStorage.setItem("ID", result.result.id);
 
         if (token) {
             window.location ="./list.html"
@@ -68,11 +72,19 @@ async function Signup() {
 }
 
 //Fonction Liste
-async function bugsList() {
+async function bugsList(ID) {
+    let userList = await usersList();
+    // let userList = localStorage.getItem("userList").split(",");
+    console.log(userList);
+
     let bugsTableBody = document.getElementById("bugListBody");
     bugsTableBody.innerHTML =""
     const token = localStorage.getItem("token");
     let userID = 0;
+
+    if (ID != 0) {
+        userID = localStorage.getItem("ID");
+    }
 
     await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/list/" + token + "/" + userID)
     .then(function (response) {
@@ -89,21 +101,80 @@ async function bugsList() {
                     <h3>${element.title}</h3>
                     <span>${element.description}</span>
                 </td>
-                <td>${element.user_id}</td>
+                <td>${userList[element?.user_id]}</td>
                 <td>
-                    <select name="" id="">
-                        <option value="">State 0</option>
-                        <option value="">State 1</option>
-                        <option value="">State 2</option>
+                    <select name="" id="${element.id}State" onchange="changeState(${element.id}, this.value)">
+                        <option value="0">State 0</option>
+                        <option value="1">State 1</option>
+                        <option value="2">State 2</option>
                     </select>
                 </td>
                 <td>
                     <button onclick="Delete(${element.id})">Supprimer</button>
                 </td>
             </tr>`
+
+            var temp = element.state;
+            var mySelect = document.getElementById(`${element.id}State`);
+
+            for(var i, j = 0; i = mySelect.options[j]; j++) {
+                if(i.value == temp) {
+                    i.setAttribute("selected", "selected");
+                    break;
+                }
+            }
+            
         });
 
         console.log(bugList);
+    })
+    .catch(error => console.log('error', error));
+}
+
+
+//Fonction Add Bug
+async function addBug() {
+
+    var formItems = {
+        "title": document.getElementById("bugAddTitle").value,
+        "description": document.getElementById("bugAddDesc").value
+    };
+
+    var options = {
+        method: 'POST',
+        body: JSON.stringify(formItems),
+      };
+
+    let bugsTableBody = document.getElementById("bugListBody");
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+
+    await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/add/" + token + "/450", options)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (result) {
+        JSON.stringify(result)
+
+        bugsTableBody.innerHTML += 
+            `<tr>
+                <td>
+                    <h3>${formItems.title}</h3>
+                    <span>${formItems.description}</span>
+                </td>
+                <td>${username}</td>
+                <td>
+                    <select name="" id="${result.result.id}State" onchange="changeState(${result.result.id}, this.value")>
+                        <option value="0">State 0</option>
+                        <option value="1">State 1</option>
+                        <option value="2">State 2</option>
+                    </select>
+                </td>
+                <td>
+                    <button onclick="Delete(${result.result.id})">Supprimer</button>
+                </td>
+            </tr>`
+
     })
     .catch(error => console.log('error', error));
 }
@@ -115,10 +186,36 @@ async function Delete(bugID) {
 
     await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/delete/" + token + "/" + bugID)
     .then(function () {
-        bugsList();
+        bugsList(0);
     })
     .catch(error => console.log('error', error));
 }
 
-// var formItems = {"title":"TestTitle",
-//     "description":"testDescription"};
+
+//Function Change State
+async function changeState(bugID, newState) {
+    const token = localStorage.getItem("token");
+    await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/state/" + token + "/" + bugID + "/" + newState)
+    .then(function (response) {
+        return response.json()
+    }) 
+    .then(function (result) {
+        console.log(result)
+    })
+    .catch(error => console.log('error', error));
+}
+
+
+//Function Liste Users
+async function usersList() {
+    const token = localStorage.getItem("token");
+    const userList =  await fetch("http://greenvelvet.alwaysdata.net/bugTracker/api/users/" + token)
+    .then(function (response) {
+        return response.json()
+    }) 
+    .then(function (result) {
+        return result.result.user;
+    })
+    .catch(error => console.log('error', error));
+    return userList
+}
