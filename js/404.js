256c5776330f0c94f620b975bfb94d0cd28e3490
Eolran <nevadasmith147@gmail.com>
2022-10-28 14:18:12 +0100

    add(404 test)

---
+++ js/404.js
@@ -0,0 +1,7 @@
+// window.addEventListener("load", error404())
+
+// function error404() {
+//     if (window.location != "./index.html", "./signup.html", "./list.html") {
+//         window.location = "./404.html";
+//     }
+// }
